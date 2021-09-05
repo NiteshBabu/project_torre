433a578675ed72123a206f5fee1ca42a5c4af1d3
Nitesh Babu Sharma <niteshbabusharma@gmail.com>
2021-09-05 18:23:59 +0530

    Changed Search Icon into Component for better usability

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import SearchIcon from '../../icons/SearchIcon'
 import "./Search.scss"
 
 function Search({ setUser, setErrText, setIsLoading }) {
@@ -24,13 +25,10 @@ function Search({ setUser, setErrText, setIsLoading }) {
     fetchData()
   }, [searchText])
 
-
   return (
     <div className="col-sm-12 text-center p-4">
       <div className="search-wrapper">
-        <svg className="search-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-        </svg>
+        <SearchIcon />
         <input type="search" className="text-center" placeholder="niteshbabusharma" onChange={e => setSearchText(e.target.value)} />
       </div>
       <small className="text-white fw-bold">Search By Username</small>
