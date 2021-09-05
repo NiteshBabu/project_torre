15d25b95dcb894995a447cf8f601413e2d642de1
Nitesh Babu Sharma <niteshbabusharma@gmail.com>
2021-09-05 11:38:09 +0530

    Included Header & Footer Components & Styling for Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import ProfilePage from './pages/profilePage'
+import Header from './components/header/Header'
+import './Main.scss'
+import Footer from './components/footer/Footer'
 
 function Main() {
   return (
-    <div>
+    <div className="bg-dark main-container">
+      <Header />
       <ProfilePage />
+      <Footer />
     </div>
   )
 }
